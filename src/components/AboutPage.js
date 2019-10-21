11c946a3fd8e9c73ae8eddc04d055908cf9feedc
bbarchu <bbarchu@gmail.com>
2019-10-21 17:10:11 -0300

    HomePage agregado - comenzando

---
+++ src/components/AboutPage.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+class AboutPage extends React.Component {
+    render() {
+        return(
+            <React.Fragment className="align-center">
+                <h2> About </h2>
+                <p> This page uses React.</p>
+            </React.Fragment>
+                
+        );
+    }
+}
+
+export default AboutPage;
---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage(){
+    return (<div className="jumbotron">
+        <h1> Foodie administration </h1>
+        <p> Administra tu aplicación </p>
+        <a href="/about">About</a>
+    </div>
+    );
+}
+
+export default HomePage;
