90d3d5efec8d15d61ac1bc95c4dbe988b7c6f254
bbarchu <bbarchu@gmail.com>
2019-10-29 20:04:30 -0300

    Editar shop exitoso

--- src/components/EditShop.js
+++ src/components/EditShop.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import TextInput from './common/TextInput';
+import { toast } from 'react-toastify';
+
 
 class EditShop extends React.Component {
     constructor(props) {
@@ -12,16 +14,32 @@ class EditShop extends React.Component {
                 address:this.props.location.props.shop.address,
                 location:this.props.location.props.shop.location,
                 category:this.props.location.props.shop.category
-            }
+            },
+            errors:{}
             
         }
         this.handleChange = this.handleChange.bind(this);
       }
     
     
-    componentWillMount(){
-        
-        
+      formIsValid(){
+        const _errors = {};
+        if(!this.state.shop.name) _errors.name= "Name is required";
+        if(!this.state.shop.category) _errors.category= "Category is required";
+        if(!this.state.shop.location) _errors.location= "Location is required";
+        if(!this.state.shop.address) _errors.address= "Address is required";
+        if(!this.state.shop.description) _errors.description= "Description is required";
+    
+    
+        this.state.errors = _errors
+        return Object.keys(this.state.errors).length === 0;
+    }  
+    
+    handleSubmit(event){
+        event.preventDefault();        
+        //if(!this.formIsValid()) return;
+        toast.success("Succesfull edit!");
+
     }
 
 
@@ -34,7 +52,7 @@ class EditShop extends React.Component {
 
     render(){
         return (
-            <form >
+            <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                     <label htmlFor={this.state.shop.id}> Id </label>
                     <div className="field">
@@ -56,6 +74,7 @@ class EditShop extends React.Component {
                 label="Name"
                 name="name"
                 value={this.state.shop.name}
+                onChange={this.handleChange}
 
                 />
                     
@@ -65,6 +84,7 @@ class EditShop extends React.Component {
                 label="Description"
                 name="description"
                 value={this.state.shop.description}
+                onChange={this.handleChange}
 
                 />
                 
@@ -73,6 +93,7 @@ class EditShop extends React.Component {
                 label="Address"
                 name="address"
                 value={this.state.shop.address}
+                onChange={this.handleChange}
 
                 />
                     
@@ -81,6 +102,7 @@ class EditShop extends React.Component {
                 label="Location"
                 name="location"
                 value={this.state.shop.location}
+                onChange={this.handleChange}
 
                 />
                     
@@ -89,8 +111,11 @@ class EditShop extends React.Component {
                 label="Category"
                 name="category"
                 value={this.state.shop.category}
+                onChange={this.handleChange}
 
                 />
+
+                <input type="submit" value="Save" className="btn btn-primary" />
 
             </form>
         );
