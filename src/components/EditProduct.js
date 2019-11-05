255260474dd2b3e8a23d54bcaff21abc8db8caec
bbarchu <bbarchu@gmail.com>
2019-11-05 17:28:46 -0300

    Se puede editar un producto correctamente

--- src/components/EditProduct.js
+++ src/components/EditProduct.js
@@ -8,42 +8,87 @@ class EditProduct extends React.Component {
         super(props);
         this.state = {
             product: {
+
                 id:this.props.location.props.product.id,
                 name:this.props.location.props.product.name,
                 description:this.props.location.props.product.description,
                 price:this.props.location.props.product.price,
-                shop: this.props.location.props.shop
+                category:this.props.location.props.product.category,
+                shop: this.props.location.props.shop.id
             },
             errors:{}
             
         }
-        this.handleChange = this.handleChange.bind(this);
+        this.handleChangeName = this.handleChangeName.bind(this);
+        this.handleChangePrice = this.handleChangePrice.bind(this);
+        this.handleChangeDescription = this.handleChangeDescription.bind(this);
+        this.handleChangeCategory = this.handleChangeCategory.bind(this);
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+
       }
     
     
       formIsValid(){
         const _errors = {};
         if(!this.state.product.name) _errors.name= "Name is required";
+        if(!this.state.product.category) _errors.category= "Category is required";
         if(!this.state.product.price) _errors.price= "Price is required";
         if(!this.state.product.description) _errors.description= "Description is required";
-    
-    
-        this.state.errors = _errors
-        return Object.keys(this.state.errors).length === 0;
+
+        this.setState({...this.state, ...{ errors: _errors}});
+        return Object.keys(_errors).length === 0;
     }  
     
     handleSubmit(event){
-        event.preventDefault();        
-        //if(!this.formIsValid()) return;
-        toast.success("Succesfull edit!");
+        event.preventDefault();
+        if(!this.formIsValid()) return;
+        fetch('http://taller2-back.herokuapp.com/api/admin/products', {
+            method: 'PUT',
+            body: JSON.stringify({
+                id: this.state.product.id,
+                shop_id:this.state.product.shop,
+                price:this.state.product.price,
+                category: this.state.product.category,
+                name: this.state.product.name,
+                description: this.state.product.description
+
+            })
+        }).then(() => { 
+            toast.success("Successfull edit!");
+        }).catch((e) => console.log(e));
 
     }
 
 
-    handleChange({target}) {
-        this.setState({product: target.value});
-        //this.setState({value: event.target.value});
+    handleChangeName({target}) {
+        const product = this.state.product
+        const update = { name : target.value}
+        const newProduct = { ...product, ...update }
+        this.setState({...this.state, ...{ product: newProduct}});
     }
+
+    handleChangeDescription({target}) {
+        const product = this.state.product
+        const update = { description : target.value}
+        const newProduct = { ...product, ...update }
+        this.setState({...this.state, ...{ product: newProduct}});
+    }
+
+    handleChangeCategory({target}) {
+        const product = this.state.product
+        const update = { category : target.value}
+        const newProduct = { ...product, ...update }
+        this.setState({...this.state, ...{ product: newProduct}});
+    }
+
+    handleChangePrice({target}) {
+        const product = this.state.product
+        const update = { price : target.value}
+        const newProduct = { ...product, ...update }
+        this.setState({...this.state, ...{ product: newProduct}});
+    }
+
 
     
 
@@ -59,7 +104,6 @@ class EditProduct extends React.Component {
                         type="text" 
                         name="id" 
                         value={ this.state.product.id } 
-                        onChange={this.handleChange}
                         className="form-control"
                         readOnly
                         />
@@ -71,7 +115,8 @@ class EditProduct extends React.Component {
                 label="Name"
                 name="name"
                 value={this.state.product.name}
-                onChange={this.handleChange}
+                error={this.state.errors.name}
+                onChange={this.handleChangeName}
 
                 />
                     
@@ -81,7 +126,8 @@ class EditProduct extends React.Component {
                 label="Description"
                 name="description"
                 value={this.state.product.description}
-                onChange={this.handleChange}
+                error={this.state.errors.description}
+                onChange={this.handleChangeDescription}
 
                 />
                 
@@ -90,7 +136,18 @@ class EditProduct extends React.Component {
                 label="Price"
                 name="price"
                 value={this.state.product.price}
-                onChange={this.handleChange}
+                onChange={this.handleChangePrice}
+                error={this.state.errors.price}
+
+                />
+
+                <TextInput
+                id="category"
+                label="Category"
+                name="category"
+                value={this.state.product.category}
+                onChange={this.handleChangeCategory}
+                error={this.state.errors.category}
 
                 />
                 
