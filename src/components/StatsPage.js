1e526e384b26ee27b4cd6f28cc01d17b117f7cea
Alejandro Pernin <ale.pernin@gmail.com>
2019-12-04 01:06:31 -0300

    Cambio el grafico de stats

--- src/components/StatsPage.js
+++ src/components/StatsPage.js
@@ -24,7 +24,54 @@ class StatsPage extends React.Component {
 			
 	margin = {top: 20, right: 20, bottom: 30, left: 40};
 	
-	state = { width: 50, users: [], shops: [] };
+	state = { 
+		width: 50, 
+		users: [],
+		users_ready: false,
+		users_done: false, 
+		shops: [],
+		options: {
+            dataLabels: {
+			  enabled: true,
+            },
+			labels: ["Users", "Deliveries"],
+            responsive: [{
+              breakpoint: 480,
+              options: {
+                chart: {
+                  width: 200
+                },
+                legend: {
+                  show: false
+                }
+              }
+            }],
+            legend: {
+                position: 'right',
+                offsetY: 0,
+                height: 230,
+			},
+			plotOptions: {
+				pie: {
+				  donut: {
+					labels: {
+					  show: true,
+					  name: {
+						show: true
+					  },
+					  value: {
+						show: true
+					  },
+					  total: {
+						  show:true
+					  }
+					}
+				  }
+				}
+			  }
+          },
+		series: [],
+    };
 	getInitialState() {
 		return { width: 50 };
 	  };
@@ -38,7 +85,7 @@ class StatsPage extends React.Component {
 		fetch(url.BASE_URL+`/api/admin/users`)
 		.then((response) => {
 		  return response.json()
-		}).then( users => this.setState ({users: users}));
+		}).then( users => this.setState ({users: users, users_ready: true}));
 
 		fetch(url.BASE_URL + '/api/shops')
 		.then((response) => {
@@ -48,13 +95,20 @@ class StatsPage extends React.Component {
 	  }
 
     render() {
+		var series = [0, 0]
+		if (this.state.users_ready && !this.state.users_done) {
+			var users = this.state.users.filter(x => x.role=="user").length
+			var deliveries = this.state.users.length - users
+			//this.setState({series: [users, deliveries]})
+			series = [users, deliveries]
+		}
         return(
             <React.Fragment className="align-center">
                 <h2> Stats </h2>
-				<Chart options={this.getOptions()} series={this.getSeries(this.state)} type="bar" width={500} height={320} />
+				<Chart options={this.state.options} series={series} type="donut" width={500} height={320} />
             </React.Fragment>
                 
-        );
+		);
     }
 }
 
