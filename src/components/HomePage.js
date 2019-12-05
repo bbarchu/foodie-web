d60b2b9b470fef4935145393af4464df2c1ced5d
bbarchu <bbarchu@gmail.com>
2019-12-04 23:57:19 -0300

    pequeño cambio en el home

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,8 +5,10 @@ function HomePage(){
     return (<div className="jumbotron">
         <h1> Foodie administration </h1>
         <p> Administra tu aplicación </p>
-        <Link to="shops" className="btn btn-primary">Shops </Link>
-        {" "}<Link to="users" className="btn btn-primary">Users</Link>
+        <Link to="shops" className="btn btn-dark">Shops </Link>
+        {" "}<Link to="users" className="btn btn-dark">Users</Link>
+        {" "}<Link to="deliveries_status" className="btn btn-dark">Deliveries</Link>
+        {" "}<Link to="balance" className="btn btn-dark">Balance</Link>
 
     </div>
     );
